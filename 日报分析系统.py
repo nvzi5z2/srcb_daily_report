fbd332fc780ec02ca822975659bd4df773e1c531
wesley cheng <wesley1993@163.com>
2024-12-10 18:59:28 +0800

    编写B款分析

--- 日报分析系统.py
+++ 日报分析系统.py
@@ -9,10 +9,13 @@ result_path=r'D:\srcb_daily_report\result'
 
 data_path=r'D:\srcb_daily_report\原始数据'
 
+#部门归属表文件名
 department_list='员工部门归属表.xlsx'
 
+#昨日日报表文件名
 yesterday_daily_report='网点鑫e贷月度指标完成情况1206.xlsx'
 
+#【浦东分行鑫e贷】客户经理营销数据文件名
 client_manager_data='【浦东分行鑫e贷】客户经理营销数据_2024-12-08.xlsx'
 
 retail_performance_data='零售市场部协同外拓及理财转介业绩报送.xlsx'
@@ -300,3 +303,103 @@ XY_Dai_Fang_Kuang_result=XY_Dai_Fang_Kuang(data_path,yesterday_daily_report,
 department_list,client_manager_data,T0_Date,type_B_data,result_path)
 
 
+def B_Kuang_Shou_Xin(data_path,yesterday_daily_report,
+department_list,client_manager_data,T0_Date,type_B_data,result_path):
+
+    yesterday_daily_report_df=pd.read_excel(data_path+'\\'+yesterday_daily_report)
+
+    yesterday_daily_report_df=yesterday_daily_report_df.iloc[1:,:]
+
+    new_columns = yesterday_daily_report_df.iloc[0,2:]  # 选择第一行作为新的列名
+
+    index = yesterday_daily_report_df.iloc[1:, 1:2].dropna()  # 假设这是你提取出来的列数据
+    index_list = index.squeeze().tolist()  # 将提取出的列转换为列表
+
+    daily_report=yesterday_daily_report_df.iloc[1:45,2:]
+
+    daily_report.index=index_list
+
+    daily_report.columns=new_columns
+
+    #第一个结果（指标）提取昨日报表指标列
+    kpi=daily_report[['指标']]
+
+    kpi.columns=['鑫e贷总授信_指标','鑫e贷放款_指标','鑫e贷授信-B款_指标']
+
+    kpi_result=kpi[['鑫e贷授信-B款_指标']]
+
+    result=pd.DataFrame(index=kpi_result.index)
+
+    result.loc[:,"鑫e贷授信-B款_指标"]=kpi_result
+
+    #第二个结果（报表数）
+    
+    type_B_df=pd.read_excel(data_path+'\\'+type_B_data,sheet_name="鑫e贷大额客户授信数据")
+
+    type_B_df=type_B_df.set_index('qianyueshijian')
+
+    type_B_df.index=pd.to_datetime(type_B_df.index)
+
+    year = pd.to_datetime(T0_Date).year  # 提取年份
+
+    month = pd.to_datetime(T0_Date).month  # 提取月份
+
+    filtered_df = type_B_df[(type_B_df.index.year == year) & (type_B_df.index.month == month)]
+
+    DD_time=filtered_df[['jingdiaokehujingli']]
+
+    DD_result = DD_time['jingdiaokehujingli'].value_counts().reset_index()
+
+    DD_result=DD_result.set_index('jingdiaokehujingli')
+
+    department_df=pd.read_excel(data_path+'\\'+department_list)
+
+    # 重命名列，确保列名一致，便于匹配
+    department_df.rename(columns={'员工姓名': 'jingdiaokehujingli', '部门': 'department'}, inplace=True)
+
+    # 合并客户经理业绩数据和部门数据
+    merged_df = DD_result.reset_index().merge(department_df, on='jingdiaokehujingli', how='left')
+    
+    # 按部门分组，计算业绩总和
+    department_totals =  merged_df.groupby('department', as_index=False)['count'].sum()
+
+    department_totals=department_totals.set_index('department',drop=True)
+
+    department_totals.columns=['鑫e贷授信-B款_报表数']
+
+    result.loc[:,"鑫e贷授信-B款_报表数"]=department_totals
+
+    #第三个结果（协同外拓）
+
+    yesterday_retail_performance=daily_report[['协同外拓']]
+
+    yesterday_retail_performance.columns=['鑫e贷总授信_昨日协同外拓','鑫e贷放款_昨日协同外拓','鑫e贷授信-B款_昨日协同外拓']
+
+    yesterday_retail_performance=yesterday_retail_performance[['鑫e贷授信-B款_昨日协同外拓']]
+
+    #写到了这里
+    
+    retail_performance_df=pd.read_excel(data_path+'\\'+retail_performance_data)
+
+    retail_performance_df=retail_performance_df.set_index('外拓日期',drop=True)
+    
+    retail_performance_df.index=pd.to_datetime(retail_performance_df.index, unit='d', origin='1899-12-30')
+
+    today_retail_df=retail_performance_df.loc[T0_Date,:]
+
+    today_retail_df=today_retail_df[['客户经理姓名','协同外拓网点','其中本人\nA款授信（户）','其中本人\nB款授信（户）']]
+
+    today_retail_df.loc[:,"鑫e贷总授信_今日协同外拓"]=today_retail_df.loc[:,"其中本人\nA款授信（户）"]+today_retail_df.loc[:,"其中本人\nB款授信（户）"]*2
+    
+    today_retail_result=today_retail_df[['协同外拓网点','鑫e贷总授信_今日协同外拓']]
+
+    today_retail_result=today_retail_result.set_index('协同外拓网点',drop=True)
+
+    newest_result=pd.concat([yesterday_retail_performance,today_retail_result],axis=1)
+
+    newest_result.loc[:, "鑫e贷总授信_协同外拓"] = (
+        newest_result.loc[:, "鑫e贷总授信_昨日协同外拓"].fillna(0) +
+        newest_result.loc[:, "鑫e贷总授信_今日协同外拓"].fillna(0)
+        )
+
+        result.loc[:,"鑫e贷总授信_协同外拓"]=newest_result.loc[:, "鑫e贷总授信_协同外拓"]
