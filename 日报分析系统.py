60f29bc9320faf34f3371a7e907210b0ef023dbe
wesley cheng <wesley1993@163.com>
2024-12-24 14:12:16 +0800

    Auto stash before merge of "main" and "origin/main"

--- 日报分析系统.py
+++ 日报分析系统.py
@@ -13,6 +13,7 @@ data_path=r'C:\Users\Wesle\Desktop\srcb_daily_report\原始数据'
 department_list='员工部门归属表.xlsx'
 
 #昨日日报表文件名
+<<<<<<< Updated upstream
 yesterday_daily_report='网点鑫e贷月度指标完成情况1223.xlsx'
 
 #【浦东分行鑫e贷】客户经理营销数据文件名
@@ -23,6 +24,18 @@ retail_performance_data='零售市场部协同外拓及理财转介业绩报送-
 type_B_data='【浦东分行鑫e贷】鑫e贷b款明细_2024-12-23.xlsx'
 
 T0_Date='2024-12-23'
+=======
+yesterday_daily_report='网点鑫e贷月度指标完成情况1220.xlsx'
+
+#【浦东分行鑫e贷】客户经理营销数据文件名
+client_manager_data='【浦东分行鑫e贷】客户经理营销数据_2024-12-22.xlsx'
+
+retail_performance_data='零售市场部协同外拓及理财转介业绩报送-6.xlsx'
+
+type_B_data='【浦东分行鑫e贷】鑫e贷b款明细_2024-12-22.xlsx'
+
+T0_Date='2024-12-22'
+>>>>>>> Stashed changes
 
 
 
